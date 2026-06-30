6644a3780e513f0a7a2127fe6f047f623d665287
Your Name <you@example.com>
2026-06-30 11:59:01 -0500

    Start clean

--- Resampler/buildFilters.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-g++ -O0 -o bin2float AudioDemo/bin2Float.cc
-g++ -O0 -o float2bin AudioDemo/float2bin.cc
--- Resampler/buildResamplers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-f77 -O0 -o unitSampleDemo MAIN.F SRINIT.F SRCONV.F
-f77 -O0 -o pcmDemo MAIN_PCM.F SRINIT.F SRCONV.F
-
